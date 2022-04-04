f123dbf1a977584c12ecdcea0d30f465b0c4a735
Nihad <ngazic@gmail.com>
2022-04-04 11:05:50 +0200

    ticets service initial tests created

---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,29 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+describe('Create new ticket: ', () => {
+
+  it('has a route handler listening to /api/tickets for POST  request', async () => {
+
+  });
+
+  it('can only be accessed if the user is signed in', async () => {
+
+  });
+
+  it('returns a status other than 401 if the user is signed in', async () => {
+
+  });
+
+  it('returns an error if an invalid title is provided', async () => {
+
+  });
+
+  it('returns an error if an invalid price is provided', async () => {
+
+  });
+
+  it('creates a ticket with valid inputs', async () => {
+
+  });
+});
